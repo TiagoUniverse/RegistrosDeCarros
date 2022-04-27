422c534be59784c8985e69e83b173f31c9605504
unknown <tiagocesar68@gmail.com>
2022-04-27 19:08:45 -0300

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegistrosDeCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
